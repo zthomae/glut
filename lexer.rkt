768b7c6c1d4be70046811f5882e725563fb41d5d
Zach Thomae <zthomae@gmail.com>
2016-01-21 13:41:48 -0600

    Add lexer

---
+++ lexer.rkt
@@ -0,0 +1,37 @@
+#lang racket/base
+
+(require parser-tools/lex)
+(require (prefix-in re- parser-tools/lex-sre))
+(require rackunit)
+
+(provide (all-defined-out))
+
+(define glut-lexer
+  (lexer
+   [whitespace
+    ;; =>
+    (glut-lexer input-port)]
+   [#\[
+    ;; =>
+    (cons '(LBR) (glut-lexer input-port))]
+   [#\]
+    ;; =>
+    (cons '(RBR) (glut-lexer input-port))]
+   [#\=
+    ;; =>
+    (cons '(EQ) (glut-lexer input-port))
+    ]
+   [(re-+ (re-~ (re-or whitespace #\[ #\] #\=)))
+    ;; =>
+    (cons `(ID ,(string->symbol lexeme))
+          (glut-lexer input-port))]
+   [(eof)
+    ;; =>
+    '()]))
+
+(let ([expr "[a][b[c]]"]
+      [stmt "[b] = next[1]"])
+  (check-equal? (glut-lexer (open-input-string expr))
+                '((LBR) (ID a) (RBR) (LBR) (ID b) (LBR) (ID c) (RBR) (RBR)))
+  (check-equal? (glut-lexer (open-input-string stmt))
+                '((LBR) (ID b) (RBR) (EQ) (ID next) (LBR) (ID |1|) (RBR))))

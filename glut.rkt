baaed5f80121012a61817521434760e69538ba9d
Zach Thomae <zthomae@gmail.com>
2016-03-08 01:08:33 -0600

    Pass debug flag result correctly

--- glut.rkt
+++ glut.rkt
@@ -12,7 +12,7 @@
      [("-p" "--print-parsed") "Print parsed program" (print-parsed #t)]
      #:args (filename)
      filename))
-  (run (parse (open-input-file file-to-run) print-parsed)
+  (run (parse (open-input-file file-to-run) (print-parsed))
        (current-input-port)
        (current-output-port))
   (void))

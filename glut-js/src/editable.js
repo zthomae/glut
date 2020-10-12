b7f614cf74c5df785c7ecb7eb877a97b4b8155de
Zach Thomae <zach@thomae.co>
2020-10-11 19:29:41 -0500

    Mildly clean up editable.js

--- glut-js/src/editable.js
+++ glut-js/src/editable.js
@@ -1,28 +1,24 @@
-module.exports = (function() {
-  'use strict';
+const makeLine = (line, index) => {
+  const lineDiv = document.createElement("div");
+  lineDiv.setAttribute("id", "line-" + (index + 1));
+  lineDiv.innerText = line;
+  return lineDiv;
+};
 
-  var makeLine = function(line, index) {
-    var lineDiv = document.createElement("div");
-    lineDiv.setAttribute("id", "line-" + (index + 1));
-    lineDiv.innerText = line;
-    return lineDiv;
-  };
-
+export const create = function(e) {
   return {
-    create: function(e) {
-      return {
-        elem: e,
-        code: "",
-        format: function() {
-          this.code = this.elem.innerText;
-          if (this.code[this.code.length - 1] != '\n') this.code += '\n';
-          var lines = this.code.split('\n').map(makeLine);
-          this.elem.innerHTML = "";
-          for (var i = 0; i < lines.length; i++) {
-            this.elem.appendChild(lines[i]);
-          }
-        }   
-      };
-    }
+    elem: e,
+    code: "",
+    format: function() {
+      this.code = this.elem.innerText;
+      if (this.code[this.code.length - 1] != "\n") {
+        this.code += "\n";
+      }
+      const lines = this.code.split("\n").map(makeLine);
+      this.elem.innerHTML = "";
+      for (let i = 0; i < lines.length; i++) {
+        this.elem.appendChild(lines[i]);
+      }
+    },
   };
-}());
+}

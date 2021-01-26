73b6d75719c9d4925a48785ef015aeb6e2cf0c60
Zach Thomae <zach@thomae.co>
2021-01-26 02:18:16 -0600

    Revert "Move glut-js into react app"
    This reverts commit 7b58749b587ca30027c54747e1aec46e9d6327e7.

--- glut-js/src/index.js
+++ glut-js/src/index.js
@@ -1,28 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as IO from "./io";
+import * as Parse from "./parse";
+import * as Machine from "./machine";
+import * as Presenter from "./presenter";
+import * as View from "./view";
 
-import * as Presenter from "./glut/presenter";
-import * as View from "./glut/view";
-
-const root = document.getElementById("legacy_root");
+const root = document.getElementById("root");
 const v = View.Create(root);
 const p = Presenter.Create(v);
 p.setDelay(10);
 Presenter.Bind(v, p);
-
-console.log(v);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
